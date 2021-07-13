876628d2146fd82b0762298984ad261f05cee200
zhulu <zhululu@weihaizixun.com>
2021-07-13 14:50:14 +0800

    test

--- binary_sensor.js
+++ binary_sensor.js
@@ -1,38 +1,40 @@
 const ha = require('./ha')
+const MqttClient = require('./MqttClient')
+const device_type = 'binary_sensor'
 
 module.exports = function (RED) {
-    const device_type = 'binary_sensor'
     RED.nodes.registerType(device_type, function (config) {
         const node = this
         // 配置
         const { server, name } = config
         // 连接MQTT
-        const mqtt = RED.nodes.getNode(server);
-        mqtt.connect()
+        const mqtt = MqttClient()
         // 获取主题信息
         const topic = ha.get_topic(device_type, name)
-
         node.on('input', function (msg) {
             const { config, state, attributes } = msg
-            // 更新配置
-            if (config) {
-                mqtt.client.publish(config_topic, Object.assign({
-                    name,
-                    state_topic: topic.state,
-                    json_attr_t: topic.attr,
-                    device_class: "motion"
-                }, config))
-                node.status({ fill: "green", shape: "ring", text: "更新配置" });
-            }
-            // 更新状态
-            if (state) {
-                mqtt.client.publish(topic.state, state)
-                node.status({ fill: "green", shape: "ring", text: "更新状态：" + state });
-            }
-            // 更新属性
-            if (attributes) {
-                mqtt.client.publish(topic.attr, attributes)
-                node.status({ fill: "green", shape: "ring", text: "更新属性" });
+            try {
+                // 更新配置
+                if (config && typeof config === 'object') {
+                    mqtt.publish(topic.config, Object.assign({
+                        name,
+                        unique_id: topic.unique_id,
+                        state_topic: topic.state,
+                        device: ha.device,
+                        json_attr_t: topic.attr,
+                        device_class: "motion"
+                    }, config))
+                }
+                // 更新状态
+                if (state) {
+                    mqtt.publish(topic.state, state)
+                }
+                // 更新属性
+                if (attributes) {
+                    mqtt.publish(topic.attr, attributes)
+                }
+            } catch (ex) {
+
             }
         })
     });
--- ha.js
+++ ha.js
@@ -1,16 +1,28 @@
 const pinyin = require("node-pinyin")
 
 module.exports = {
+    id: 'home-assistant',
     object_id(name) {
         let arr = pinyin(name, { style: 'normal' })
         return arr.map(ele => ele[0]).join('_')
     },
+    // 获取MQTT设备信息
+    get device() {
+        return {
+            name: 'HA-MQTT',
+            identifiers: ['635147515'],
+            manufacturer: 'shaonianzhentan',
+            model: 'HomeAssistant',
+            sw_version: '0.1'
+        }
+    },
     get_topic(type, name) {
-        const object_id = this.object_id(name)
+        const object_id = 'ha_mqtt_' + this.object_id(name)
         const topic = `ha-mqtt/${type}/${object_id}/`
         return {
+            unique_id: type + '_' + object_id,
             // 配置
-            config: `${topic}config`,
+            config: `homeassistant/${type}/${object_id}/config`,
             // 状态
             state: `${topic}state`,
             // 属性
@@ -18,6 +30,5 @@ module.exports = {
             // 控制
             command: `${topic}set`
         }
-    },
-    
+    }
 }
