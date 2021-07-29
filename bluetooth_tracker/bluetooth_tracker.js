d0029f19e7be53f49c28ee71a9948d1a3119e4b6
shaonianzhentan <ilovevar@gmail.com>
2021-07-29 16:24:36 +0800

    增加蓝牙检测功能

--- bluetooth_tracker/bluetooth_tracker.js
+++ bluetooth_tracker/bluetooth_tracker.js
@@ -1,56 +1,36 @@
-const { exec } = require('child_process');
-const ping = require('ping');
+const { spawn } = require('child_process');
+
+function log() {
+    console.log(`【${new Date().toLocaleString()}】`, ...arguments)
+}
 
 module.exports = function (RED) {
     RED.nodes.registerType('ha-mqtt-bluetooth_tracker', function (cfg) {
         RED.nodes.createNode(this, cfg);
         const node = this
-        const { ip, mac } = cfg
-        // 计数器
-        const count = 0
-        const ble_tracker = () => {
-            exec(`hcitool name '${mac}'`, { timeout: 5000 }, (error, stdout, stderr) => {
-                if (error) {
-                    console.log('蓝牙检测出现错误：', error);
-                    // 如果错误了，则重启蓝牙
-                    exec('hciconfig hci0 up')
-                    return;
+        const ls = spawn('node', [__dirname + '/tracker.js', JSON.stringify(cfg)]);
+
+        ls.stdout.on('data', (data) => {
+            log(data.toString());
+            try {
+                data = JSON.parse(data)
+                if ('payload' in data) {
+                    node.send({ payload: data.payload })
+                } else if ('error' in data) {
+                    node.status({ fill: "red", shape: "ring", text: JSON.stringify(data.error) });
+                } else if ('isAlive' in data) {
+                    node.status({ fill: "green", shape: "ring", text: `检测到IP${data.isAlive ? '在线' : '离线'}` });
                 }
-                // 检测到有人
-                if (stdout) {
-                    count = 0
-                    node.send({ payload: 'home' })
-                    node.status({ fill: "green", shape: "ring", text: '检测到蓝牙设备' });
-                } else {
-                    // 没有检测到设备时，计数器加一
-                    count += 1
-                }
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-            });
-        }
-        // 15秒执行一次
-        setTimeout(() => {
-            // 如果4次没检测到设备，则宣布没人
-            if (count >= 4) {
-                node.send({ payload: 'not_home' })
+            } catch {
             }
-            if (ip) {
-                // 检测IP
-                ping.sys.probe(ip, function (isAlive) {
-                    // 当前设备存在
-                    if (isAlive) {
-                        count = 0
-                        node.send({ payload: 'home' })
-                    } else {
-                        // 如果IP不在线，则检测蓝牙
-                        ble_tracker()
-                    }
-                    node.status({ fill: "green", shape: "ring", text: `检测到IP${isAlive ? '在线' : '离线'}` });
-                }, { timeout: 2 });
-            } else {
-                ble_tracker()
-            }
-        }, 15000)
+        });
+
+        ls.stderr.on('data', (data) => {
+            node.status({ fill: "red", shape: "ring", text: `stderr: ${data}` });
+        });
+
+        ls.on('close', (code) => {
+            node.status({ fill: "red", shape: "ring", text: `child process exited with code ${code}` });
+        });
     })
 }
---
+++ bluetooth_tracker/tracker.js
@@ -0,0 +1,71 @@
+const { exec } = require('child_process');
+const ping = require('ping');
+
+const { ip, mac } = JSON.parse(process.argv[2])
+
+function log(data) {
+    console.log(JSON.stringify(data))
+}
+// 在家检测时间
+const homeTime = 15000
+// 离家检测时间
+const not_homeTime = 10000
+// 计数器
+let count = 0
+// 蓝牙检测
+const ble_tracker = () => {
+    // 如果4次没检测到设备，则宣布没人
+    if (count >= 4) {
+        log({ payload: 'not_home' })
+    }
+    return new Promise((resolve, reject) => {
+        exec(`hcitool name '${mac}'`, { timeout: 5000 }, (error, stdout, stderr) => {
+            if (error) {
+                log({ error });
+                // 如果错误了，则重启蓝牙
+                exec('hciconfig hci0 up')
+                reject()
+                return;
+            }
+            // 检测到有人
+            if (stdout) {
+                log({ payload: 'home' })
+                resolve()
+            } else {
+                // 没有检测到设备时，计数器加一
+                reject()
+            }
+        });
+    })
+}
+// IP检测
+const ip_tracker = () => {
+    ping.sys.probe(ip, function (isAlive) {
+        if (isAlive == null) return;
+        log({ isAlive })
+        // 当前设备存在
+        if (isAlive) {
+            count = 0
+            setTimeout(() => {
+                ip_tracker()
+            }, homeTime)
+            log({ payload: 'home' })
+        } else {
+            // 如果IP不在线，则检测蓝牙
+            ble_tracker().then(() => {
+                count = 0
+                setTimeout(() => {
+                    ip_tracker()
+                }, homeTime)
+            }).catch(() => {
+                count += 1
+                // 如果蓝牙检测出错，则10秒检测一次
+                setTimeout(() => {
+                    ip_tracker()
+                }, not_homeTime)
+            })
+        }
+    }, { timeout: 10 });
+}
+
+ip_tracker()
