1cf5e959e63844896cd1dfc967b18e3f484cd636
zhulu <zhululu@weihaizixun.com>
2021-08-27 11:18:41 +0800

    调整键盘测试逻辑

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -1,10 +1,16 @@
-const { spawn } = require('child_process');
+const { spawn, execSync } = require('child_process');
 
 module.exports = function (RED) {
     RED.nodes.registerType('ha-mqtt-keyboard', function (config) {
         RED.nodes.createNode(this, config);
         const node = this
         const inputDevice = config.name
+        
+        // 显示当前输入设备
+        node.on('input', function (msg) {
+            const payload = execSync('cat /proc/bus/input/devices').toString()
+            node.send([null, { payload }])
+        })
 
         function monitorKeyboard() {
             const ls = spawn('node', [__dirname + '/input.js', inputDevice]);
@@ -15,12 +21,12 @@ module.exports = function (RED) {
                 if ('code' in data) {
                     node.status({ fill: "green", shape: "ring", text: `键码：${data.code}` });
                     // console.log(data)
-                    node.send({
+                    node.send([{
                         payload: {
                             dev: inputDevice,
                             ...data
                         }
-                    })
+                    }, null])
                 } else {
                     node.status(data)
                 }
